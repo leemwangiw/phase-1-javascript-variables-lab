f48fbb6887e8a8a1fda39868df40452be8b7979d
lee mwangi <lee.mwangi1@student.moringaschool.com>
2024-06-26 17:53:15 +0300

    commiting variableslab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+function companyName() {
+    const companyName = 'Scuber';
+}
+function mostProfitableNeighborhood() {
+    let mostProfitableNeighborhood = 'Chelsea';
+}
+function companyCeo() {
+    let companyCeo = 'Susan Smith';
+}
